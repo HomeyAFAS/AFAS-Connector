56c1f22927a9513a4f9b6bd26d492a103c43dc32
Patrick Hopman <p.hopman@afas.nl>
2016-12-02 14:40:06 +0100

    HTTP Request get connector

--- com.athom.afas/app.js
+++ com.athom.afas/app.js
@@ -1,11 +1,26 @@
 "use strict";
-var token = "<token><version>1</version><data>B1F7F01F7D7441628DA647C51D52B1D583E0C5CC448114BC3227F9A84AD5DD77</data></token>";
+const request = require('request');
 function init() {
-	
-
-	Homey.log("Hello world!");
-	Homey
-	
+	Homey.manager('flow').on('action.get_connector', function( callback, args ){
+		var token = Homey.manager('settings').get( 'token' );
+		var url = 'https://32772.afasonlineconnector.nl/profitrestservices/connectors/Pocket_Subjects';
+		
+		request({
+			headers: {
+				'authorization': 'AfasToken ' + new Buffer(token).toString('base64') 
+			},
+			uri: url + '?skip=0&take=1&filterfieldids=subjecttype&filtervalues=41&operatortypes=1'
+		}, function(error, response, body) {
+			if (error) {
+				Homey.log('error getting poa');
+				return [];
+			} else {
+				var response = JSON.parse(body);
+				Homey.log(response);
+			}
+		});
+	    callback( null, true ); // we've fired successfully
+	});
 }
 
 module.exports.init = init;
