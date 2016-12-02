1773a943db8623bf8343a0a4be313b022d824250
Patrick Hopman <p.hopman@afas.nl>
2016-12-02 10:17:43 +0100

    Initial commit

---
+++ com.athom.afas/app.js
@@ -0,0 +1,11 @@
+"use strict";
+var token = "<token><version>1</version><data>B1F7F01F7D7441628DA647C51D52B1D583E0C5CC448114BC3227F9A84AD5DD77</data></token>";
+function init() {
+	
+
+	Homey.log("Hello world!");
+	Homey
+	
+}
+
+module.exports.init = init;
