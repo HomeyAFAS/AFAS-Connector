7211230bf377916993699101130616aaac0c00f4
Patrick Hopman <patrickhopman@me.com>
2016-12-03 15:55:55 +0100

    Autocomplete op connector naam

--- com.athom.afas/app.js
+++ com.athom.afas/app.js
@@ -13,17 +13,35 @@ function init() {
 			if (installed !== true) return callback('BetterLogic is not installed', null)
 		})
 
-		connector.get_connector( args, function( err, response ){
+		connector.get_connector( args.get_connector + "connectors/", "skip=0&take=1&" + args.filter, function( err, response ){
 			if (err) {
-				Homey.log(err)
+				Homey.log(err);
 			} else {
-				betterLogic.put('/patrick/' + response['title'], null, function (err, result) {
+				var result = response[0];
+				betterLogic.put('/patrick/' + result['title'], null, function (err, result) {
 					if (err) Homey.log(err);
-					Homey.log(response);
-					callback(null, true)
+					callback(null, true);
 				});
 			}
 		});
+	});
+
+	Homey.manager('flow').on('action.get_connector.arg_name.get_connector', function( callback, args ){
+		connector.get_connector( "metainfo", "", function( err, response){
+			if (err) {
+				Homey.log(err);
+			} else {
+				var getConnectors = response['getConnectors'];
+
+				// filter items to match the search query
+				getConnectors = getConnectors.filter(function(item){
+					return ( item.id.toLowerCase().indexOf( args.query.toLowerCase() ) > -1 )
+				});
+
+				callback( null, getConnectors ); // err, results
+			}
+		});
+
 	});
 }
 
--- com.athom.afas/lib/connector.js
+++ com.athom.afas/lib/connector.js
@@ -1,24 +1,24 @@
 const request = require('request');
 
-exports.get_connector = function(args, callback){
+exports.get_connector = function(connector, filter, callback){
 	var token = Homey.manager('settings').get( 'token' );
-	var url = 'https://32772.afasonlineconnector.nl/profitrestservices/connectors/' + args.get_connector;
+	var url = 'https://32772.afasonlineconnector.nl/profitrestservices/' + connector;
 	request({
 		headers: {
 			'authorization': 'AfasToken ' + new Buffer(token).toString('base64')
 		},
-		uri: url + '?skip=0&take=1&' + args.filter,
+		uri: url + '?' + filter,
 		timeout: 1500
-	}, function(error, response, body) {
-		if (error) {
-			Homey.log(error);
-			callback(error, body);
+	}, function(err, response, body) {
+		if (err) {
+			Homey.log(err);
+			callback(err, body);
 		} else {
 			if (len(response.rows) > 0){
 				var response = JSON.parse(body);
-				callback(null, response.rows[0]);
+				callback(null, response.rows);
 			} else {
-				error = "No rows found";
+				callback("No rows found", null);
 			}
 		}
 	});
