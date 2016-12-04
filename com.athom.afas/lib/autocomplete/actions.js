c863207d00bea932988c5fa0213c6f6b23c1b25d
Patrick Hopman <patrickhopman@me.com>
2016-12-04 14:37:55 +0100

    Getconnector met spraak toegevoegd

--- com.athom.afas/lib/autocomplete/actions.js
+++ com.athom.afas/lib/autocomplete/actions.js
@@ -4,7 +4,18 @@ const connector = require('../connector.js');
 
 module.exports = {
 	createAutocompleteActions: function () {
-		Homey.manager('flow').on('action.get_connector.arg_name.get_connector', function( callback, args ){
+		Homey.manager('flow').on('action.get_connector_better_logic.arg_name.get_connector', function( callback, args ){
+			connector.getConnector( "metainfo", "", function( err, response ){
+				if (err) Homey.log(err); return callback( 'No connectors found', null );
+
+				var getConnectors = response['getConnectors'];
+				getConnectors = getConnectors.filter( function( item ) { return ( item.id.toLowerCase().indexOf( args.query.toLowerCase() ) > -1 ) } );
+				return callback( null, getConnectors );
+			});
+
+		});
+
+		Homey.manager('flow').on('action.get_connector_speech.arg_name.get_connector', function( callback, args ){
 			connector.getConnector( "metainfo", "", function( err, response ){
 				if (err) Homey.log(err); return callback( 'No connectors found', null );
 
--- com.athom.afas/lib/flow/actions.js
+++ com.athom.afas/lib/flow/actions.js
@@ -16,11 +16,23 @@ module.exports = {
 				if (err) Homey.log(err); return callback('There is something wrong with the connector', false);
 
 				var result = response[0];
-				betterLogic.put('/patrick/' + result['title'], null, function ( err, result ) {
+				betterLogic.put('/' + args.variable_name + '/' + result['title'], null, function ( err, result ) {
 					if (err) Homey.log(err); return callback('There is something wrong with BetterLogic', false);
 					return callback(null, true);
 				});
 			});
 		});
+
+		Homey.manager('flow').on('action.get_connector_speech', function( callback, args ){
+			connector.getConnector( args.get_connector + "connectors/", "skip=0&take=1&" + args.filter, function( err, response ){
+				if (err) Homey.log(err); return callback('There is something wrong with the connector', false);
+
+				var result = response[0];
+				Homey.manager('speech-output').say( __("Er is een prio 2 ingeschoten met als omschrijving", result['title'] ) );
+				return callback(null, true);
+			});
+		});
+
+		Homey.manager('speech-output').say( __("hello_world") );
 	}
 };
